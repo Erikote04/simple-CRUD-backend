39439bd12fdc33f2b46c225cb761245332f6f93d
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-21 12:18:00 +0100

    First we connect to the database and then we run the server

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
-
 mongoose
     .connect('mongodb+srv://example:s6cUX597LfYnRNvu@backenddb.h2ticen.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log('Connected!'));
+    .then(() => {
+        console.log('Connected!')
+        return
+    })
+    .then(() => {
+        app.listen(3000, () => {
+        console.log("Server running on port 3000")
+        })
+    })
+    .catch(() => console.log("Connection failed!"))
