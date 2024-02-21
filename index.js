0706b2695958229ddd84948e0772a89060f84d47
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-21 12:09:14 +0100

    Connecting to the database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.get('/', (req, res) => {
@@ -8,3 +9,7 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
+
+mongoose
+    .connect('mongodb+srv://example:s6cUX597LfYnRNvu@backenddb.h2ticen.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('Connected!'));
