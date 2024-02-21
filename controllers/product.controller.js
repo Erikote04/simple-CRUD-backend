0afafffe9646344a696ca7ce6ba3f735c08f3d26
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-21 13:37:34 +0100

    Creating products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -18,3 +18,12 @@ const getProduct = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+const createProduct = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
