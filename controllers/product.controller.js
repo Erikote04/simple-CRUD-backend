fd89bc0a348918a887985ffa4b2fe03f0d26fe14
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-21 13:38:30 +0100

    Updating products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -27,3 +27,19 @@ const createProduct = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
