ac820ec069344203b70f480c5bdac0be0ea25608
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-21 13:33:33 +0100

    Preparing the project for using a router

--- index.js
+++ index.js
@@ -6,17 +6,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use("/api/products", productRoute);
+
 app.get('/', (req, res) => {
     res.send('Hello World')
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
 })
 
 mongoose
